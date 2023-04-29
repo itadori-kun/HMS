a1be09f2f2a987ebf1309016326c8436ce89a25f
Godwin <boiikechukwu.godwin@gmail.com>
2023-04-29 05:34:21 -0400

    had to follow a naming convention

--- Models/Godwin/Branch.js
+++ Models/Godwin/Branch.js
@@ -3,12 +3,12 @@ const Schema = mongoose.Schema
 
 const BranchSchema = new Schema({
   name: { type: String, required: true },
-  hospital: { type: Schema.Types.ObjectId, ref: 'Hospital', required: true },
+  hospital: { type: Schema.Types.ObjectId, ref: 'hospitals', required: true },
   address: { type: String, required: true },
   phone: [{ type: String, required: true }],
   admin: { type: String, required: true },
   staff_total: { type: String, required: true }
 })
 
-const Branch = mongoose.model('Branch', BranchSchema)
+const Branch = mongoose.model('branches', BranchSchema)
 module.exports = Branch
--- Models/Godwin/Receipt.js
+++ Models/Godwin/Receipt.js
@@ -3,26 +3,27 @@ const Schema = mongoose.Schema
 
 const ReceiptSchema = new Schema(
   {
-    card_no: { type: Schema.Types.ObjectId, ref: 'Patient', required: true },
+    card_no: { type: Schema.Types.ObjectId, ref: 'patients', required: true },
     due_date: { type: Date },
-    dept: {
-      type: Schema.Types.ObjectId,
-      ref: 'Department',
-      required: true
-    },
+
     payment_analysis: [
       {
+        dept: {
+          type: Schema.Types.ObjectId,
+          ref: 'departments',
+          required: true
+        },
         descp: { type: String, required: true },
-        amount: { type: Number, required: true }
+        amount: { type: Number, required: true },
+        payment_mode: { type: String, required: true },
+        payment_status: { type: String, required: true }
       }
     ],
     vat: { type: String, required: true },
-    payment_mode: { type: String, required: true },
-    payment_status: { type: String, required: true },
     total_amount: { type: Number, required: true }
   },
   { timestamps: true }
 )
 
-const Receipt = mongoose.model('Receipt', ReceiptSchema)
+const Receipt = mongoose.model('receipts', ReceiptSchema)
 module.exports = Receipt
