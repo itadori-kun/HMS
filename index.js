74763339e24f598d410955485c33db90d95bd935
Ciscoflash <abrahamjude1999@gmail.com>
2023-04-30 18:07:47 +0100

    hospital department and branch routes done

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const app = express()
 const mongoose = require('mongoose')
 // Port in Use
 const PORT = 3001
+const hospital = require('./routes/Hospital')
 
 // Setting up the Mongodb connection
 const DBConnection = require('./config/DBConnection')
@@ -19,6 +20,9 @@ app.use(express.json())
 
 // routes
 app.use('/employee', require('./routes/Employee'))
+app.use('/hospital', hospital)
+app.use('/branch', require('./routes/Branch'))
+app.use('/department', require('./routes/Department'))
 
 mongoose.connection.once('open', function () {
   console.log('connected to mongodb')
--- routes/Hospital.js
+++ routes/Hospital.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const app = express.Router()
-const hospitalModel = require('../Models/Hospital')
-app
-.route('/')
-.post(async(req, res)=>{
+const hospitalModel = require('../Models/Hospital');
+const Hospital = require('../Models/Hospital');
+
+app.route('/').post(async(req, res)=>{
   const hospital = new hospitalModel(req.body)
   if(!hospital) return res.sendStatus(404).json({msg:"invalid hospital"})
 const duplicate = await hospitalModel.findOne({email: hospital.email})
@@ -13,7 +13,7 @@ await hospital.save()
 res.json({msg:"hospital created successfully", code:201, hospital})
 })
 
-.get(async(req, res)=>{
+app.route('/').get(async(req, res)=>{
   const hospital = await hospitalModel.find()
   if(!hospital) return res.sendStatus(404).json({msg:"hospital not found"})
   res.json({
@@ -23,11 +23,56 @@ res.json({msg:"hospital created successfully", code:201, hospital})
   })
 })
 
+app.route('/:id')
 .put(async(req, res)=>{
+if(!req.params?.id){
+return res.sendStatus(400).json({message:"bad request"})
+}
+try {
+  const hospital = await hospitalModel.findById({_id: req.params.id}).exec()
+if(!hospital) return sendStatus(404).json({msg:"hospital does not exist"})
 
+let data = {...hospital._doc, ...req.body}
+hospital.overwrite(data)
+await hospital.save()
+res.json({
+  code:200,
+  msg:"hospital updated successfully",
+  hospital
+})
+  
+} catch (error) {
+  return res.json(error)
+}
+})
+.delete(async(req, res)=>{
+  if(!req?.params?.id) return res.sendStatus(400).json({msg:"bad request"})
+  try {
+    const hospital = await hospitalModel.findOne({_id:req.params.id}).exec()
+    if(!hospital) return res.sendStatus(404).json({msg:"hospital not found"})
+    const result  = await hospitalModel.findByIdAndDelete({_id:req.params.id})
+    return res.json({
+      code:200,
+      msg: "hospital deleted successfully",
+      result
+    })
+  } catch (error) {
+    return res.json(error)
+  }
 })
 
-.delete(async(req, res)=>{
-  
+.get(async(req, res)=>{
+  if(!req?.params?.id) return res.sendStatus(400).json({msg:"bad request"})
+  try {
+    const hospital = await hospitalModel.findOne({_id:req.params.id}).exec()
+    if(!hospital) return res.sendStatus(400).json({msg:"hosptial not found"})
+    return res.json({
+      code:200,
+      msg:"single hospital found",
+      hospital
+    })
+  } catch (error) {
+    return res.json(error)
+  }
 })
 module.exports = app
