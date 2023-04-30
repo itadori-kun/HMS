50c3fb37519bb0fef912fb88563f3a5e85c83868
Godwin Boi-Ikechukwu <boiikechukwu.godwin@gmail.com>
2023-04-30 14:07:52 -0400

    update employee

---
+++ Models/Employee.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const EmployeeSchema = new Schema({
+  first_name: { type: String },
+  last_name: { type: String },
+  avatar: { type: String },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  phone: [{ type: String }],
+  address: { type: String },
+  status: {
+    type: String,
+    enum: ['available', 'leave', 'break']
+  },
+  staff_type: { type: String, enum: ['permanent', 'contract'] },
+  role: { type: String },
+  branch: { type: String, ref: 'branches' },
+  department: {
+    type: String,
+    ref: 'departments'
+  }
+})
+
+const Employee = mongoose.model('employees', EmployeeSchema)
+module.exports = Employee
