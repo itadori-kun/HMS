8e3983c7648d7d8af2ad8bc6dbab1494cb2bdaa6
Godwin <boiikechukwu.godwin@gmail.com>
2023-05-09 11:11:27 -0400

    can now post ward and patients info to nurse model and also made a route to remove ward and patients from the schema

--- routes/Nurse.js
+++ routes/Nurse.js
@@ -23,9 +23,7 @@ app.route('/').get(async (req, res) => {
 app
   .route('/:id')
   .post(async (req, res) => {
-    if (!req?.body) {
-      return res.status(400).json({ msg: 'Branch not found' })
-    }
+    const { ward_no, branch_id, patients_incharge_of } = req.body
     let speciality = ''
     const employee = await Employee.findOne({ _id: req.params.id }).exec()
     if (!employee || employee.role !== 'nurse') {
@@ -35,34 +33,52 @@ app
       emp_id: req?.params?.id
     }).exec()
     try {
-      if (!get_nurse_info?.emp_id) {
+      // create a new nurse info if the particular nutse has none yet
+      if (!get_nurse_info) {
         speciality = new Nurse({
           emp_id: req.params.id,
-          ward_no: req.body.ward_no,
-          patients_incharge_of: req.body.patients_incharge_of,
-          branch_id: req.body.branch_id
+          ward_no: ward_no,
+          patients_incharge_of: patients_incharge_of,
+          branch_id: branch_id
         })
         await speciality.save()
-      } else if (
-        get_nurse_info &&
-        !get_nurse_info?.ward_no?.includes(req.body.ward_no)
-      ) {
-        get_nurse_info.ward_no.push(req.body.ward_no)
-        get_nurse_info.patients_incharge_of.push(req.body.patients_incharge_of)
-        speciality = await get_nurse_info.save()
-      } else if (get_nurse_info?.ward_no?.includes(req.body.ward_no)) {
-        get_nurse_info.patients_incharge_of.push(req.body.patients_incharge_of)
-        speciality = await get_nurse_info.save()
+        res.status(201).json({
+          msg: 'Ward and patients assigned ',
+          data: speciality
+        })
       }
-
-      res.status(201).json({
-        msg: 'Specialty assigned ',
-        data: speciality
-      })
+      // if only ward is being assigned
+      if (get_nurse_info && ward_no) {
+        const update_ward = get_nurse_info.ward_no.filter(
+          obj => obj._id == ward_no
+        ).length
+        if (!update_ward) {
+          get_nurse_info.ward_no.push(ward_no)
+          speciality = await get_nurse_info.save()
+          res.status(201).json({ msg: 'Ward Assiged', data: speciality })
+        } else {
+          res.status(200).json({ msg: 'Already exist' })
+        }
+      }
+      // if only patient is being assigned
+      if (get_nurse_info && patients_incharge_of) {
+        const update_patient = get_nurse_info.patients_incharge_of.filter(
+          obj => obj._id == patients_incharge_of
+        ).length
+        if (!update_patient) {
+          get_nurse_info.patients_incharge_of.push(patients_incharge_of)
+          speciality = await get_nurse_info.save()
+          res.status(201).json({ msg: 'Patient Assiged', data: speciality })
+        } else {
+          res.status(200).json({ msg: 'Already exist' })
+        }
+      }
     } catch (err) {
+      console.error(err)
       res.status(500).json({ msg: 'Something went wrong' })
     }
   })
+
   .get(async (req, res) => {
     if (!req?.params?.id) {
       return res.status(400).json({ msg: 'Failed request' })
@@ -98,28 +114,30 @@ app.route('/:id/rmv').post(async (req, res) => {
   const { ward_no, patients_incharge_of } = req.body
   const update = await Nurse.findOne({
     emp_id: req?.params?.id
-  } ).exec()
-  if ( !update ) {
-    res.status(400).json({msg:"Record not found"})
+  }).exec()
+  if (!update) {
+    res.status(400).json({ msg: 'Record not found' })
   }
 
   try {
     if (ward_no) {
-      for (let i = 0; i < update.ward_no.length; i++) {
-        if (update?.ward_no[i]?._id == ward_no) {
-          update.ward_no.pull(ward_no)
-          await update.save()
-        }
+      const found_ward_no = update.ward_no.findIndex(obj => obj._id == ward_no)
+
+      if (found_ward_no !== -1) {
+        update.ward_no.splice(found_ward_no, 1)
+        await update.save()
+        res.status(200).json({ msg: 'Deleted successfully', update })
       }
     } else if (patients_incharge_of) {
-      for (let i = 0; i < update.patients_incharge_of.length; i++) {
-        if (update?.patients_incharge_of[i]?._id == patients_incharge_of) {
-          update.patients_incharge_of.pull(patients_incharge_of)
-          await update.save()
-        }
+      const found_patient = update.patients_incharge_of.findIndex(
+        obj => obj._id == patients_incharge_of
+      )
+      if (found_patient !== -1) {
+        update.patients_incharge_of.splice(found_patient, 1)
+        await update.save()
+        res.status(200).json({ msg: 'Deleted successfully', update })
       }
     }
-    res.status(200).json({ msg: 'Deleted successfully', update })
   } catch (err) {
     res.status(500).json({ msg: 'Something went wrong' })
   }
