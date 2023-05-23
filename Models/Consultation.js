73b1556154d8f5b94865177ddff73a242d3d4238
vincent <uveghobamien@gmail.com>
2023-05-23 17:19:28 +0100

    consultation model update

--- Models/Consultation.js
+++ Models/Consultation.js
@@ -5,7 +5,9 @@ const consultationSchema = mongoose.Schema({
   payment_status:{type:String, enum:['paid','notpaid', 'pending']},
   diagnosis:{type:String},
   prescription:{type:String},
-  medication_id:{type:String}
+  medication_id:{type:String},
+  doctor_seen:{type:Boolean,default:false,required:true},
+  nurse_seen:{type:Boolean,default:false,required:true}
 })
 
 const Consultation = mongoose.model('consultations', consultationSchema)
