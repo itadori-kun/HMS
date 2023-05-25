141cdd5a16f6121cc723db319bb283d834c58471
Ciscoflash <abrahamjude1999@gmail.com>
2023-05-25 10:48:39 +0100

    new change on the prescription route

--- Models/Prescription.js
+++ Models/Prescription.js
@@ -1,13 +1,19 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const prescriptionSchema = mongoose.Schema({
-  patient_id:{type:String, ref:"patients" , required:true},
-  date_of_diagnosis:{type:Date},
-  presriptiopn:{type:Array},
-  notes:{type:String, required:true},
-  doctor_name: {type:String, ref:'employees', required:true},
-  doctor_initials:{type:String, required:true}
-})
+  patient_id: { type: String, ref: "patients", required: true },
+  date_of_diagnosis: { type: Date },
+  presription: [
+    {
+      frequency: { type: String, required: true },
+      name: { type: String, required: true },
+      strength: { type: String, required: true },
+    }
+  ],
+  notes: { type: String, required: true },
+  doctor_name: { type: String, ref: "employees", required: true },
+  doctor_initials: { type: String, required: true },
+});
 
-const Prescription = mongoose.model('prescriptions', prescriptionSchema)
+const Prescription = mongoose.model("prescriptions", prescriptionSchema);
 
-module.exports = Prescription
+module.exports = Prescription;
