51151674bdea502349df6ebc5898091c856168a7
Ciscoflash <abrahamjude1999@gmail.com>
2023-07-09 19:18:54 +0100

    commiting changes

--- routes/Pharmacy.js
+++ routes/Pharmacy.js
@@ -1,28 +1,26 @@
-const express = require('express')
-const app = express.Router()
-const Pharmacy = require('../Models/Pharmacy')
+const express = require("express");
+const app = express.Router();
+const Pharmacy = require("../Models/Pharmacy");
 
-
-app.route('/').get(async (req, res) => {
+app.route("/").get(async (req, res) => {
   try {
-    let filter = {}
+    let filter = {};
 
-    let { branch_id, emp_id, name } = req.query
-    
-    if (branch_id) filter.branch_id = branch_id
-    if (emp_id) filter.emp_id = emp_id
-    if (name) filter.name = name
+    let { branch_id, emp_id, name } = req.query;
+
+    if (branch_id) filter.branch_id = branch_id;
+    if (emp_id) filter.emp_id = emp_id;
+    if (name) filter.name = name;
 
     let pharmacyFilter = await Pharmacy.find(filter)
-    .populate("drug_id")
-    .populate("emp_id")
-    .populate("branch_id")
+      .populate("drug_id")
+      .populate("emp_id")
+      .populate("branch_id");
     res.json({
       msg: "successfull",
       data: pharmacyFilter,
-      code: 200
-
-    })
+      code: 200,
+    });
     // const get_all_pharmacies = await Pharmacy.find()
     // if (!get_all_pharmacies)
     //   return res.json({
@@ -36,73 +34,72 @@ app.route('/').get(async (req, res) => {
     //   code: 401
     // })
   } catch (err) {
-
-    console.log(err)
-    res.status(500).send(err)
+    console.log(err);
+    res.status(500).send(err);
     res.json({
-      msg: 'failed to retrieve pharmacy'
-    })
+      msg: "failed to retrieve pharmacy",
+    });
   }
-})
+});
 
-app.route('/create').post(async (req, res) => {
+app.route("/create").post(async (req, res) => {
   if (!req.body)
     return res.json({
       code: 400,
-      msg: 'request body is missing or incomplete'
-    })
+      msg: "request body is missing or incomplete",
+    });
   try {
-    const duplicate = await Pharmacy.findOne({ "name": req.body.name })
+    const duplicate = await Pharmacy.findOne({ name: req.body.name });
     if (duplicate)
       return res.json({
-        msg: 'pharmacy already exist,create pharmacy using a new name',
-        code: 401
-      })
+        msg: "pharmacy already exist,create pharmacy using a new name",
+        code: 401,
+      });
 
-    const pharmacy = new Pharmacy(req.body)
-    await pharmacy.save()
+    const pharmacy = new Pharmacy(req.body);
+    await pharmacy.save();
     res.json({
-      msg: 'successful',
+      msg: "successful",
       data: pharmacy,
-      code: 200
-    })
+      code: 200,
+    });
   } catch (err) {
-    res.status(500).send(err)
-    console.log(err)
+    res.status(500).send(err);
+    console.log(err);
     res.json({
-      msg: 'failed to create new pharmacy'
-    })
+      msg: "failed to create new pharmacy",
+    });
   }
-})
+});
 
 app
-  .route('/:id')
+  .route("/:id")
   .get(async (req, res) => {
     if (!req.params.id)
       return res.json({
         code: 400,
-        msg: 'request body is missing or incomplete'
-      })
+        msg: "request body is missing or incomplete",
+      });
 
     try {
-      let pharmacy = await Pharmacy.findById(req.params.id)
+      let pharmacy = await Pharmacy.findById(req.params.id);
       if (!pharmacy)
         return res.json({
-          msg: 'pharmacy does not exist',
-          code: 404
-        })
+          msg: "pharmacy does not exist",
+          code: 404,
+        });
 
       res.json({
-        msg: 'successful',
+        msg: "successful",
         data: pharmacy,
-        code: 200
-      })
+        code: 200,
+      });
     } catch (err) {
-      res.status(500).send(err)
-      console.log(err)
+      res.status(500).send(err);
+      console.log(err);
       res.json({
-        msg: 'failed to retrieve pharmacy'
-      })
+        msg: "failed to retrieve pharmacy",
+      });
     }
   })
 
@@ -110,61 +107,60 @@ app
     if (!req.params.id)
       return res.json({
         code: 400,
-        msg: 'request body is missing or incomplete'
-      })
+        msg: "request body is missing or incomplete",
+      });
     try {
-      const pharmacy = await Pharmacy.findById(req.params.id)
-      if (!pharmacy) return res.json({
+      const pharmacy = await Pharmacy.findById(req.params.id);
+      if (!pharmacy)
+        return res.json({
+          code: 401,
+          msg: "pharmacy  does not exist",
+        });
+      let update_pharmacy = { ...pharmacy._doc, ...req.body };
+      pharmacy.overwrite(update_pharmacy);
 
-        code: 401,
-        msg: 'pharmacy  does not exist'
-      })
-      let update_pharmacy = { ...pharmacy._doc, ...req.body }
-      pharmacy.overwrite(update_pharmacy)
-
-      await pharmacy.save()
+      await pharmacy.save();
       res.json({
-        msg: ' update successful',
+        msg: " update successful",
         data: pharmacy,
-        code: '200'
-      })
+        code: "200",
+      });
     } catch (err) {
-      res.status(500).send(err)
-      console.log(err)
+      res.status(500).send(err);
+      console.log(err);
       res.json({
-        msg: 'failed to update pharmacy '
-      })
+        msg: "failed to update pharmacy ",
+      });
     }
   })
 
   .delete(async (req, res) => {
     if (!req.params.id)
       return res.json({
-        msg: 'request body is missing or incomplete'
-      })
+        msg: "request body is missing or incomplete",
+      });
 
     try {
-      let pharmacy = await Pharmacy.findById(req.params.id)
+      let pharmacy = await Pharmacy.findById(req.params.id);
       if (!pharmacy)
         return res.json({
-          msg: 'pharmacy does not exist',
-          code: 404
-        })
+          msg: "pharmacy does not exist",
+          code: 404,
+        });
 
-      await Pharmacy.findByIdAndDelete(req.params.id)
+      await Pharmacy.findByIdAndDelete(req.params.id);
       res.json({
-        msg: 'phaarmacy deleted',
-        code: 200
-      })
+        msg: "phaarmacy deleted",
+        code: 200,
+      });
     } catch (err) {
-      res.status(500).send(err)
+      res.status(500).send(err);
       res.json({
-        msg: 'failed to delete pharmacyr '
-      })
+        msg: "failed to delete pharmacyr ",
+      });
 
-      console.log(err)
+      console.log(err);
     }
-  })
+  });
 
-
-module.exports = app
+module.exports = app;
